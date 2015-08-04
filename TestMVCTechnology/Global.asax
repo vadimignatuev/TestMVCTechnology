b1d7805478600da626ef62e7d4f2ee053045e993
vadimignatuev <vadimigna@yandex.ru>
2015-08-05 00:12:22 +0300

    Commit

---
+++ TestMVCTechnology/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestMVCTechnology.MvcApplication" Language="C#" %>
---
+++ TestMVCTechnology/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestMVCTechnology.Startup))]
+namespace TestMVCTechnology
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
